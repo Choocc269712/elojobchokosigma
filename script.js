938f124a11e83de0365234f61c6f3534320abea5
Choocc269712 <gabrielgegeu@gmail.com>
2026-08-01 07:02:09 -0300

    asdasfdasd

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-async function testar() {
-
-    const { data, error } = await supabase
-        .from("jobs")
-        .select("*");
-
-    if (error) {
-        console.error(error);
-        alert("Erro: " + error.message);
-        return;
-    }
-
-    console.log(data);
-    alert("Conectado com sucesso!");
-}
-
-testar();
+alert("script.js carregou!");
