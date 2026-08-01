23ce7952a385609b4929db48e6d83672cd21b902
Choocc269712 <gabrielgegeu@gmail.com>
2026-08-01 07:13:57 -0300

    asd

--- script.js
+++ script.js
@@ -2,57 +2,19 @@ const cards = document.getElementById("cards");
 
 async function atualizar() {
 
-const { data: jobs, error } = await db
-    .from("jobs")
-    .select("*")
-    .order("fim");
+    const { data, error } = await db
+        .from("jobs")
+        .select("*");
+
+    console.log("DATA:", data);
+    console.log("ERROR:", error);
 
     if (error) {
-        console.error(error);
+        alert(error.message);
         return;
     }
 
-    cards.innerHTML = "";
-
-    jobs.forEach(job => {
-
-        const diff = new Date(job.fim) - new Date();
-
-        let tempo;
-
-        if (diff <= 0) {
-
-            tempo = "Finalizado";
-
-        } else {
-
-            const horas = Math.floor(diff / 1000 / 60 / 60);
-            const minutos = Math.floor(diff / 1000 / 60) % 60;
-            const segundos = Math.floor(diff / 1000) % 60;
-
-            tempo = `${horas}h ${minutos}m ${segundos}s`;
-        }
-
-        cards.innerHTML += `
-            <div class="card">
-                <div class="info">
-                    <div class="label">Tempo restante</div>
-                    <div class="value">${tempo}</div>
-                </div>
-
-                <div class="info">
-                    <div class="label">Boost</div>
-                    <div class="value">${job.boost}</div>
-                </div>
-
-                <div class="info">
-                    <div class="label">Boostando</div>
-                    <div class="value">${job.booster}</div>
-                </div>
-            </div>
-        `;
-    });
-
+    alert(`Encontrados ${data.length} elojobs`);
 }
 
 atualizar();
