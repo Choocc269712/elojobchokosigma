686f2d2554d3763926b8d122e0cba3b81537b6ae
Choocc269712 <gabrielgegeu@gmail.com>
2026-08-01 07:00:38 -0300

    asdffa

--- script.js
+++ script.js
@@ -1,73 +1,17 @@
-let jobs = JSON.parse(localStorage.getItem("jobs")) || [];
+async function testar() {
 
-const cards = document.getElementById("cards");
+    const { data, error } = await supabase
+        .from("jobs")
+        .select("*");
 
-function atualizar(){
+    if (error) {
+        console.error(error);
+        alert("Erro: " + error.message);
+        return;
+    }
 
-    cards.innerHTML="";
-
-    jobs.forEach(job=>{
-
-        let fim = new Date(job.fim);
-
-        let agora = new Date();
-
-        let diff = fim-agora;
-
-        let tempo;
-
-        if(diff<=0){
-
-            tempo="Finalizado";
-
-        }else{
-
-            let horas=Math.floor(diff/1000/60/60);
-
-            let minutos=Math.floor(diff/1000/60)%60;
-
-            let segundos=Math.floor(diff/1000)%60;
-
-            tempo=`${horas}h ${minutos}m ${segundos}s`;
-
-        }
-
-        cards.innerHTML+=`
-
-        <div class="card">
-
-        <div class="info">
-
-        <div class="label">Tempo restante</div>
-
-        <div class="value">${tempo}</div>
-
-        </div>
-
-        <div class="info">
-
-        <div class="label">Boost</div>
-
-        <div class="value">${job.boost}</div>
-
-        </div>
-
-        <div class="info">
-
-        <div class="label">Boostando</div>
-
-        <div class="value">${job.booster}</div>
-
-        </div>
-
-        </div>
-
-        `;
-
-    });
-
+    console.log(data);
+    alert("Conectado com sucesso!");
 }
 
-atualizar();
-
-setInterval(atualizar,1000);
+testar();
---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://lgnluhzejcmthejfsqgz.supabase.co";
+const SUPABASE_KEY = "sb_publishable_OwXKHvF5uKvd5Hc_P0AMrA_P0jk3JOA";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
